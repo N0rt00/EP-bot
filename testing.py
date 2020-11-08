128e7eb9a16887cfb967b84080d9d7e77d706fa6
N0rt00 <73914057+N0rt00@users.noreply.github.com>
2020-11-09 08:38:50 +1100

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,10 +6,15 @@ for i in range(1):
     im = ImageGrab.grab()
     im.save('fullscreen.png')
 im = Image.open ('fullscreen.png')
-left = -1000
+
+#these need to be changed to crop the image to the question
+
+left = -1000 
 top =  -1000
 right = 600
 bottom = 300
+
+
 im1 = im.crop((left, top, right, bottom))
 im1.save('fullscreen2.png')
 
@@ -24,6 +29,4 @@ im1.save('fullscreen2.png')
 
 
 
-
-    #if os.path.exists("fullscreen.png"):
-        #os.remove("fullscreen.png")
+    
