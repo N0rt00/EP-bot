3b719d30d14bdba0e373044e9253168aa89f5b65
N0rt00 <73914057+N0rt00@users.noreply.github.com>
2020-11-16 12:06:18 +1100

    Delete screenshot wip.py

--- screenshot wip.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-import pyscreenshot as ImageGrab
-for i in range(20)
-  im = ImageGrab.grab()
-  im.save('fullscreen.png')
-  time.sleep(5)
-  if os.path.exists("fullscreen.png"):
-    os.remove("fullscreen.png")
