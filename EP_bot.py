d974202b249cf8c3f31c1b27ac55b82184e669ea
VoidsGod <73452871+VoidsGod@users.noreply.github.com>
2021-02-11 18:13:53 +1100

    Update EP_bot.py

--- EP_bot.py
+++ EP_bot.py
@@ -1,19 +1,77 @@
 inputList = """
-danser to dance
-elle joue she plays; she is playing
-faire de l'exercice to do exercise
-faire du sport to play sports
-il joue he plays; he is playing
-ils jouent they play (masculine plural); they are playing
-ils jouent au tennis they are playing tennis (masc)
-ils jouent; elles jouent they play (feminine plural); they are playing
-je joue I play
-je joue au basket; je joue au basketball I play basketball
-je monte I climb; I go up; I am climbing; I am going up
-jouer to play
-marcher to walk
-monter to climb
-muscler to work out
+avril April
+dimanche Sunday
+février February
+juin June
+mardi Tuesday
+mars March
+le soir evening
+septembre September
+janvier January
+octobre October
+novembre November
+l'après-midi afternoon
+le midi midday
+l'hiver Winter
+l'été Summer
+le français French
+l'éducation musicale Music
+la cour de récréation playground
+la cantine canteen
+les mathématiques; les maths Maths; mathematics
+le printemps Spring
+août August
+décembre December
+les sciences Science
+l'art dramatique Drama
+le cours class
+jeudi Thursday
+vendredi Friday
+demain tomorrow
+une minute a minute
+la salle de classe classroom
+l'anglais English
+juillet July
+le matin morning
+lundi Monday
+mai May
+mercredi Wednesday
+l'école school; the school
+aujourd'hui today
+une heure an hour
+un élève; une élève student
+le collège middle school
+la bibliothèque library
+les arts plastiques Art
+la technologie Technology
+le gymnase gymnasium
+samedi Saturday
+les matières subjects
+l'automne Autumn
+le professeur; le prof male teacher
+la vie de classe homegroup lesson
+la menuiserie Woodwork
+la horticulture Horticulture
+Il est quelle heure? What's the time?
+la note score; mark; grade
+quelle heure est-il? What's the time?; What time is it?; What is the time?
+Il est vingt heures It"s 8pm; It is 8pm
+Il est huit heures moins le quart It's a quarter to 8; It is quarter to 8
+un collégien male student
+une collégienne female student
+l'EPS Physical Education; PE
+l'histoire-géographie Hums; humanities
+la professeure; la prof female teacher
+le terrain de sport sports oval; sports field
+Dans mon collège il y a un gym In my school there's a gym; In my school there is a
+gym
+Dans mon collège il y a une cantine In my school there's a canteen; In my school there is a
+canteen
+dans mon collège il y a un terrain de sport in my school there's a sports ground; in my school
+there is a sport ground
+Dans mon collège il y a un terrain de foot In my school there is a football ground; In my school
+there's a footy ground
+Dans mon collège il y a un terrain de tennis In my school there's
 """
 
 inputList = inputList.splitlines()
@@ -37,7 +95,11 @@ print("Go To Task")
 # Answers Questions
 while 1 == 1:
     print('running')
+    go = 0
     if 'game?' in driver.current_url:
+        go = 1
+    if go == 1:
+        try:
             time.sleep(0.01)
             inputElement = driver.find_elements_by_xpath("//span[@id='question-text']")
             text = ''
@@ -68,5 +130,7 @@ while 1 == 1:
                 userinput.send_keys(new)
                 userinput = driver.find_element_by_xpath("//button[@id='submit-button']")
                 userinput.click()
+        except:
+            go = 0
         
 
